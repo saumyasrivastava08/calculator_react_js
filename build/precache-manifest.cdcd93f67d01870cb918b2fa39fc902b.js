f72e0d629793d7b0262bfc837db46fbcbe3b801a
Saumya Srivastava <57553757+saumyasrivastava08@users.noreply.github.com>
2020-10-01 22:55:57 +0530

    Add files via upload

---
+++ build/precache-manifest.cdcd93f67d01870cb918b2fa39fc902b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068bc1413e6aabbfcdafee301f5a6b97",
+    "url": "/calculator_react_js/index.html"
+  },
+  {
+    "revision": "05dbc37d5728b421761b",
+    "url": "/calculator_react_js/static/css/main.af0af1b5.chunk.css"
+  },
+  {
+    "revision": "957638ea8982320dc693",
+    "url": "/calculator_react_js/static/js/2.e3675be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator_react_js/static/js/2.e3675be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05dbc37d5728b421761b",
+    "url": "/calculator_react_js/static/js/main.2b25c606.chunk.js"
+  },
+  {
+    "revision": "cb3d0d33c7b84487f2cf",
+    "url": "/calculator_react_js/static/js/runtime-main.cdf54bd9.js"
+  }
+]);
